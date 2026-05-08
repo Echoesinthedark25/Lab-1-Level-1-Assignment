27b22b351c9c6413201005eff220c880f8b3048a
Echoesinthedark25 <000937901@mohawkcollege.ca>
2026-05-08 12:53:00 -0400

    Add sprites and inputs

--- Lab 1 - Level One/Assets/Scripts/Player.cs
+++ Lab 1 - Level One/Assets/Scripts/Player.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D rb2d;
+    public Animator animator;
+    public float speedX = 1f;
+
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(moveX) > 0.1f)
+        {
+            float force = moveX * speedX;
+            rb2d.AddForceX(force, ForceMode2D.Force);
+        }
+        animator.SetFloat("moveSpeedX", Mathf.Abs(moveX));
     }
 }
